2662167ff40a9d1b903d15472c1bf769eb5b2453
ofmla <o_mojical@hotmail.com>
2022-04-04 21:42:02 -0300

    update GMT scripts and add new dark-light figs

--- data/gmt_scripts/plot_3d.sh
+++ data/gmt_scripts/plot_3d.sh
@@ -4,8 +4,6 @@ gmt begin true
 	gmt set PS_CHAR_ENCODING = ISOLatin1+
 	gmt set FONT_ANNOT_PRIMARY=10    
 	gmt set FONT_LABEL=10
-	gmt set COLOR_BACKGROUND=255/0/0
-	gmt set COLOR_FOREGROUND=255/0/0
 	gmt set COLOR_NAN=200/215/137
 	gmt set PS_PAGE_ORIENTATION=PORTRAIT
 	gmt set FORMAT_FLOAT_OUT=%.12lg
@@ -20,7 +18,7 @@ gmt begin true
 	gmt grdimage xyz.grd -Cmola.cpt -JX -E60/30 -R0/40.5/0/73.5 -p145/45
 	gmt grdcontour filtered.nc -C0.25  -Wthinnest,black, -JX -p145/45
 	echo "34 51 N" | gmt pstext -JX -p145/45 -F+f24p+jLM
-	echo "33.5 52 \255" | gmt pstext -JX -p145/45 -F+f36p,Symbol+jLM
+	echo "33.5 52 \255" | gmt pstext -JX -p145/45 -F+f36p,Symbol+jLM 
 	gmt grdview filtered.nc -JX6.75/12.25 -JZ3 -Cmola.cpt -Ifiltered_i.nc -p145/45 -R0/40.5/0/73.5/-5/0 -N-5 -Qi200 -Y-11 -Wc -V -B"x5+lHorizontal coordinate x (km)" -B"y5+lHorizontal coordinate y (km)" -B"z1+lDepth (km)" -BnESwZ
 	gmt colorbar -Cmola.cpt -DJRM+o6c/7.5c+w8c/0.4c -Bx0.5 -By+lkm
 gmt end show
